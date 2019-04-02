92cbeca8f919fb5c3593ebed28c6d419b2e2b2c9
Srihithareddyp7 <33138730+Srihithareddyp7@users.noreply.github.com>
2019-04-01 22:15:41 -0500

    Update ignite_java.sh

--- ignite_java.sh
+++ ignite_java.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #nohup /opt/IGNITE-2_6/bin/ignite.sh /opt/IGNITE-2_6/A-Ignite/config/example-ignite.xml > /dev/null & 
-java -cp /opt/IGNITE-2_6/A-Ignite/target/apacheIgnite-1.0-SNAPSHOT-jar-with-dependencies.jar /opt/IGNITE-2_6/A-Ignite/targetcom.ignite.servicegrid.ServicesExample
+java -cp apacheIgnite-1.0-SNAPSHOT-jar-with-dependencies.jar targetcom.ignite.servicegrid.ServicesExample
